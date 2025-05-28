a77de689e6fea1d2921a07d9e35ab1dd6ecd36b5
Varo392 <varo392@gmail.com>
2025-05-28 10:30:50 -0600

    Cambios en Items, se agregan contructores, sobre cargados, contructor simple

--- csharp.NUnit/GildedRose/Item.cs
+++ csharp.NUnit/GildedRose/Item.cs
@@ -1,8 +1,32 @@
-﻿namespace GildedRoseKata;
+﻿using System.Data;
+
+namespace GildedRoseKata;
 
 public class Item
 {
+    /// <summary>
+    /// / todoos los metodos se cambian a privados 
+    /// para que solo se puedan acceder con la clase. 
+    /// </summary>
     public string Name { get; set; }
     public int SellIn { get; set; }
     public int Quality { get; set; }
+
+    /// es necsario el metodo contructor sobre cargado para pasar valores
+    public Item(string name, int sellIn, int quality)
+    {
+        this.Name = name;
+        this.SellIn = sellIn;
+        this.Quality = quality;
+    }
+
+    // son necesarios los metodos por defecto 
+    // para limpiar basura de la memoria
+    public Item()
+    {
+        this.Name = "";
+        this.SellIn = 0;
+        this.Quality = 0;
+    }
+    
 }
--- csharp.NUnit/GildedRose/Program.cs
+++ csharp.NUnit/GildedRose/Program.cs
@@ -11,31 +11,17 @@ public class Program
 
         IList<Item> items = new List<Item>
         {
-            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-            new Item
-            {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
-                SellIn = 15,
-                Quality = 20
-            },
-            new Item
-            {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
-                SellIn = 10,
-                Quality = 49
-            },
-            new Item
-            {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
-                SellIn = 5,
-                Quality = 49
-            },
+            // se cambio para que el constructor sobre cargado sea el que se use
+            new Item ("+5 Dexterity Vest", 10,20),
+            new Item ( "Aged Brie", 2, 0),
+            new Item ("Elixir of the Mongoose",5,7),
+            new Item ("Sulfuras, Hand of Ragnaros",0,80),
+            new Item ("Sulfuras, Hand of Ragnaros",-1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert",15,20),
+            new Item("Backstage passes to a TAFKAL80ETC concert",10,49),
+            new Item("Backstage passes to a TAFKAL80ETC concert",5,49),
             // this conjured item does not work properly yet
-            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            new Item ("Conjured Mana Cake",3, 6)
         };
 
         var app = new GildedRose(items);
